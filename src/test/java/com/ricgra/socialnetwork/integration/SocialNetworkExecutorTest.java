1e8dbaf0aa1dac6525a6b7f3c803837ef4cc73ed
ricgra <riccardo.grandi@esignworld.com>
2020-12-12 11:13:19 +0100

    add description to tests

--- src/test/java/com/ricgra/socialnetwork/integration/SocialNetworkExecutorTest.java
+++ src/test/java/com/ricgra/socialnetwork/integration/SocialNetworkExecutorTest.java
@@ -15,12 +15,12 @@ public class SocialNetworkExecutorTest {
         socialNetworkExecutor = new SocialNetworkExecutor();
     }
 
-    @Test(priority = 1)
+    @Test(priority = 1, description = "Should return null for a invalid command in input")
     public void shouldNotRunNotExistingCommand() {
         Assert.assertNull(socialNetworkExecutor.runCommand(""));
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2, description = "Should create user's posts")
     public void shouldPostMessagesWithoutAnyError() {
         Assert.assertEquals(socialNetworkExecutor.runCommand("Alice -> I love the weather today"), "");
         Assert.assertEquals(socialNetworkExecutor.runCommand("Bob -> Damn! We lost!"), "");
@@ -28,18 +28,18 @@ public class SocialNetworkExecutorTest {
         Assert.assertEquals(socialNetworkExecutor.runCommand("Charlie -> I'm in New York today! Anyone wants to have a coffee?"), "");
     }
 
-    @Test(priority = 3)
-    public void shouldGetMessagesWithoutAnyError() {
+    @Test(priority = 3, description = "Should get and print user's posts")
+    public void shouldGetPostsWithoutAnyError() {
         Assert.assertEquals(socialNetworkExecutor.runCommand("Alice").split("\n").length, 1);
         Assert.assertEquals(socialNetworkExecutor.runCommand("Bob").split("\n").length, 2);
     }
 
-    @Test(priority = 4)
+    @Test(priority = 4, description = "Should follow a user")
     public void shouldFollowWithoutAnyError() {
         socialNetworkExecutor.runCommand("Charlie follows Bob");
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, description = "Should print a user's wall posts")
     public void shouldPrintWallWithoutAnyError() {
         socialNetworkExecutor.runCommand("Charlie walls");
     }
